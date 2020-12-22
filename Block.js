246d0067721d76141b7add69a236021dd0c172a4
adwait2007 <74483768+adwait2007@users.noreply.github.com>
2020-12-22 17:14:09 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -11,10 +11,9 @@ class Block{
         World.add(world, this.body);
       }
       display(){
-        var angle = this.body.angle;
+       
         push();
         translate(this.body.position.x, this.body.position.y);
-        rotate(angle);
         rectMode(CENTER);
         rect(0, 0, this.width, this.height);
         pop();
